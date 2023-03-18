6eace74e8235a5d7560e1f85499f9365b53356fc
subphrenic6 <128237885+subphrenic6@users.noreply.github.com>
2023-03-18 10:10:48 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 mkdir -p /etc/hysteria
 version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 
-wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-amd64
+wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/apernet/hysteria/releases/download/v1.3.4/hysteria-linux-amd64
 
 chmod 755 /etc/hysteria/hysteria
 
